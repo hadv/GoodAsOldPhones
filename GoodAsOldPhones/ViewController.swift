5e1084e512b7c84694ec7be3beb48486140da3e9
hadv <dvietha@gmail.com>
2016-07-15 14:11:46 +0700

    Initial Commit

---
+++ GoodAsOldPhones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Dang Viet Ha on 7/15/16.
+//  Copyright © 2016 AgileBit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
