1b2d36a7e935f7048c12054a5d64fa452f1da9dc
hadv <dvietha@gmail.com>
2016-07-19 13:50:44 +0700

    LEVEL 2: Outlets and Actions

---
+++ GoodAsOldPhones/ProductViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProductViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Dang Viet Ha on 7/15/16.
+//  Copyright © 2016 AgileBit. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productNameLabel.text = "1937 Desk Phone"
+        productImageView.image = UIImage(named: "phone-fullscreen3")
+    }
+    
+    @IBAction func addToCartPressed(sender: AnyObject) -> Void {
+        print("Button tapped!")
+    }
+}
