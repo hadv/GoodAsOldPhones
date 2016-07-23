9ba4da355c191ce2eefda313ed3443e7da84e724
hadv <dvietha@gmail.com>
2016-07-23 10:15:39 +0700

    LEVEL 3: Scroll Views

---
+++ GoodAsOldPhones/ContractViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ContractViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Dang Viet Ha on 7/23/16.
+//  Copyright © 2016 AgileBit. All rights reserved.
+//
+
+import UIKit
+
+class ContractViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(scrollView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.contentSize = CGSizeMake(375, 800)
+    }
+    
+}
